f27f00bf80e1d8aad4121785eab1b11d0d30a95b
William Y. Feng <womogenes2@gmail.com>
2026-02-14 13:47:41 -0800

    Add FPGA demo

--- hardware/fpga/hdl/top_level.sv
+++ hardware/fpga/hdl/top_level.sv
@@ -11,13 +11,34 @@ module top_level (
     input wire esp_sig_p,
     input wire esp_sig_n
 );
-    logic sysclk_200mhz_passthrough;
-    clk_wiz clk_inst(
-        .sysclk_p(sysclk_p),
-        .sysclk_n(sysclk_n),
-        .sysclk_200mhz_passthrough(sysclk_200mhz_passthrough)
+    // clk_wiz doesn't work yet idt
+    // logic sysclk_200mhz_passthrough;
+    // clk_wiz clk_inst (
+    //     .sysclk_p(sysclk_p),
+    //     .sysclk_n(sysclk_n),
+    //     .sysclk_200mhz_passthrough(sysclk_200mhz_passthrough)
+    // );
+
+    // TEST SCRIPT
+    wire clk_100mhz;
+    wire sysclk_locked;
+
+    clkwiz_100mhz sysclk_wiz (
+        .sysclk_p   (sysclk_p),
+        .sysclk_n   (sysclk_n),
+        .clk_100mhz (clk_100mhz),
+        .locked     (sysclk_locked)
     );
-    
+
+    logic [25:0] count;
+
+    always_ff @(posedge clk_100mhz) begin
+        count <= count + 1;
+
+        if (count == 0) begin
+            led <= led + 1;
+        end
+    end
 
 endmodule
 `default_nettype wire
